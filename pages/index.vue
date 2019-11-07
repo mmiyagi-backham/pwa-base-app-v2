7ebde3c0fe576f432c6cae719d4591c1cb367f8c
Michihiro Miyagi <mmiyagi@mmiyagiBackhamMbp.local>
2019-11-07 13:52:56 +0900

    edit message

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,8 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        pwa-base-app-v2
-      </h1>
-      <h2 class="subtitle">
-        My wicked Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">pwa-base-app-v2</h1>
+      <h2 class="subtitle">My wicked Nuxt.js projects YEAH!!</h2>
     </div>
   </div>
 </template>
